bdcc8a98f20bcad868cb63141b4c7e6e61ca5de0
Jacob Rizer <jriz908@gmail.com>
2016-11-15 06:47:43 +0000

    Peer.java edited online with Bitbucket

--- BitTorrent Client/src/Main/Peer.java
+++ BitTorrent Client/src/Main/Peer.java
@@ -1,20 +1,44 @@
-//Jacob Rizer
-
 package Main;
 
+/**
+ * 
+ * @author Jacob Rizer
+ *
+ * This class represents a peer that we will 
+ * download from. Each peer contains an ID
+ * IP address and port. 
+ */
 public class Peer {
-
+	
 	private String peer_ID;
 	private String IP;
 	private int port;
+	
+	/**
+	 * when we unchoke the peer we set this
+	 * to true.
+	 */
 	private boolean unchoked;
 	
+	/**
+	 * Constructor with ID
+	 * 
+	 * @param peer_ID 
+	 * @param IP
+	 * @param port
+	 */
 	public Peer(String peer_ID, String IP, int port){
 		this.peer_ID = peer_ID;
 		this.IP = IP;
 		this.port = port;
 	}
 	
+	/**
+	 * Constructor without ID
+	 * 
+	 * @param IP
+	 * @param port
+	 */
 	public Peer(String IP, int port){
 		this.IP = IP;
 		this.port = port;
