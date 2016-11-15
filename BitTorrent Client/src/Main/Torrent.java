5cd2dd4727ad7d1593eb7dbc7155752c64835e60
Jacob Rizer <jriz908@gmail.com>
2016-11-15 06:46:38 +0000

    Torrent.java edited online with Bitbucket

--- BitTorrent Client/src/Main/Torrent.java
+++ BitTorrent Client/src/Main/Torrent.java
@@ -34,6 +34,10 @@ import GivenTools.*;
 
 public class Torrent {
 	
+	/**
+	 * Some public static fields. Mainly
+	 * literal bytebuffers that we use for communication.
+	 */
 	
 	public static final ByteBuffer PEERS = ByteBuffer.wrap(new byte[] { 'p', 'e', 'e', 'r', 's' });
 	public static final ByteBuffer INTERVAL = ByteBuffer.wrap(new byte[] { 'i', 'n', 't', 'e', 'r', 'v', 'a', 'l' });
@@ -54,42 +58,92 @@ public class Torrent {
 	public static final ByteBuffer REQUEST = ByteBuffer.wrap(new byte[]{0, 0, 0, 13, 6});
 	public static final ByteBuffer PIECE = ByteBuffer.wrap(new byte[]{0, 0, 0, 0, 7});
 	
+	/**
+	 * The length that we will use for one block.
+	 * 2^14 bytes.
+	 */
 	public static final int LENGTH = 16384 ;
 	
 	
 	
-	
+	/**
+	 * The torrentInfo object obtained from the torrent file
+	 */
 	private TorrentInfo torrentInfo;
 	
-	
+	/**
+	 * Stream for writing bytes to the file
+	 */
 	private FileOutputStream fos;
 	
 	private int port = 6881;
+	
+	/**
+	 * These will change as the download proceeds.
+	 * Also used for the tracker messages.
+	 */
 	private int downloaded;
 	private int uploaded;
 	private int left;
+	
+	/**
+	 * For tracker communication.
+	 * The event field will change as the download
+	 * progresses.
+	 */
 	private String event;
 	private String peerID;
 	private String infoHash;
 	
+	/**
+	 * The URL that we use to contact the tracker.
+	 */
 	private URL trackerURL;
 	
+	/**
+	 * The tracker response will contain a map
+	 * as well as the list of peers we will download
+	 * from.
+	 */
 	private Map<ByteBuffer,Object> trackerResponse;
 	private List<Peer> peers;
 	
+	/**
+	 * The socket we will use as well as the streams
+	 * we will use to communicate with the peer.
+	 */
 	private Socket socket;
 	private DataInputStream input;
 	private DataOutputStream output;
 	
+	/**
+	 * Some buffers for peer communication
+	 */
 	private ByteBuffer responseBuffer;
 	private ByteBuffer requestBuffer;
 	
+	/**
+	 * Piece index. Increments by 1 after each
+	 * piece is successfully downloaded.
+	 */
 	private int index = 0;
-	private int numBlocks;
+	
+	/**
+	 * Whenever we get to a new piece we need
+	 * to use this to advance the index number
+	 * and verify the hash of a new completed
+	 * piece.
+	 */
 	private boolean firstBlock;
 	
 	
-	
+	/**
+	 * Constructor
+	 * 
+	 * @param ti - torrentInfo object we made from the torrentFile argument
+	 * @param fileName - The name we will use for our new file. Given as an argument.
+	 * @throws FileNotFoundException
+	 */
 	public Torrent(TorrentInfo ti, String fileName) throws FileNotFoundException{
 		this.torrentInfo = ti;
 		this.fos = new FileOutputStream(new File(fileName));
@@ -100,7 +154,6 @@ public class Torrent {
 		this.left = torrentInfo.file_length;
 		this.event = "";
 		
-		this.numBlocks = ti.piece_length/LENGTH;
 		firstBlock = true;
 		
 		this.requestBuffer = ByteBuffer.allocate(17);
@@ -109,7 +162,12 @@ public class Torrent {
 		
 	}
 	
-	//set the tracker URL that will be used to create a connection with the tracker
+	/**
+	 * set the tracker URL that will be used to create a connection with the tracker.
+	 * Event is left out here because it's the first request we send.
+	 * 
+	 * @throws MalformedURLException
+	 */
 	public void setTrackerURL() throws MalformedURLException{
 		String urlString = this.torrentInfo.announce_url.toString() + 
 						   "?info_hash=" + this.infoHash + 
@@ -123,6 +181,11 @@ public class Torrent {
 		trackerURL = new URL(urlString);
 	}
 	
+	/**
+	 * The URL we use when we start the download.
+	 * 
+	 * @throws MalformedURLException
+	 */
 	public void setTrackerURLEventStarted() throws MalformedURLException{
 		
 		this.event = "started";
@@ -140,6 +203,11 @@ public class Torrent {
 		trackerURL = new URL(urlString);
 	}
 	
+	/**
+	 * The URL we use when we finish the download.
+	 * 
+	 * @throws MalformedURLException
+	 */
 	public void setTrackerURLEventCompleted() throws MalformedURLException{
 		
 		this.event = "completed";
@@ -157,6 +225,11 @@ public class Torrent {
 		trackerURL = new URL(urlString);
 	}
 	
+	/**
+	 * The URL we use when we are ready to close everything.
+	 * 
+	 * @throws MalformedURLException
+	 */
 	public void setTrackerURLEventStopped() throws MalformedURLException{
 		
 		this.event = "stopped";
@@ -174,7 +247,13 @@ public class Torrent {
 		trackerURL = new URL(urlString);
 	}
 	
-	//send a request to the tracker to get the list of peers
+	/**
+	 * send a request to the tracker to get the list of peers.
+	 * The URL is made with the setTrackerURL functions.
+	 * 
+	 * @throws IOException
+	 * @throws BencodingException
+	 */
 	@SuppressWarnings("unchecked")
 	public void sendRequestToTracker() throws IOException, BencodingException{
 		
@@ -197,7 +276,11 @@ public class Torrent {
 	   
 	}
 	
-	//iterate through list of peers received from trackers to obtain the ones that have the -RU prefix
+	/**
+	 * iterate through list of peers received from trackers to obtain the ones that have the -RU prefix
+	 * 
+	 * @throws UnsupportedEncodingException
+	 */
 	public void setPeers() throws UnsupportedEncodingException{
 		
 		List trackerPeers = (List) trackerResponse.get(PEERS);
@@ -222,7 +305,14 @@ public class Torrent {
 		}
 	}
 	
-	//download from fastest peer
+	/**
+	 * download from fastest peer. We ping the peers to determine 
+	 * the fastest one.
+	 * 
+	 * @throws IOException
+	 * @throws BencodingException
+	 * @throws InterruptedException
+	 */
 	public void start() throws IOException, BencodingException, InterruptedException{
 		Peer p = getFastestPeer(peers);
 		//Peer p = peers.get(0);
@@ -230,6 +320,13 @@ public class Torrent {
 		download(p);
 	}
 	
+	/**
+	 * Clean up at the end. Send the stopped event to the tracker
+	 * and then close up sockets and streams.
+	 * 
+	 * @throws IOException
+	 * @throws BencodingException
+	 */
 	public void close() throws IOException, BencodingException{
 		
 		setTrackerURLEventStopped();
@@ -240,6 +337,13 @@ public class Torrent {
 		socket.close();
 	}
 	
+	/**
+	 * Given a list of peers, ping each one 10 times
+	 * and return the peer with the lowest RTT average.
+	 * 
+	 * @param peers - list of peers with -RU prefix
+	 * @return the peer with the fastest RTT.
+	 */
 	public Peer getFastestPeer(List<Peer> peers){
 		
 		double min = Double.MAX_VALUE;
@@ -280,6 +384,12 @@ public class Torrent {
 		
 	}
 	
+	/**
+	 * Send a ping to a peer to test its RTT
+	 * 
+	 * @param peer - the peer we are contacting.
+	 * @return the time it takes to get a ping back (in milliseconds).
+	 */
 	public double pingPeer(Peer peer){
 		
 		double result = 0;
@@ -306,13 +416,25 @@ public class Torrent {
 		return result;
 	}
 	
-	//begin the process of downloading from peer
+	/**
+	 * begin the process of downloading from peer.
+	 * Open up streams, perform handshake, send interested,
+	 * receive unchoke, and then finally perform the downloading of all 
+	 * the pieces.
+	 * 
+	 * @param peer
+	 * @throws IOException
+	 * @throws BencodingException
+	 * @throws EOFException
+	 * @throws InterruptedException
+	 */
 	public void download(Peer peer) throws IOException, BencodingException, EOFException, InterruptedException{
 
 		String ip = peer.getIP();
 		int port = peer.getPort();
 
-
+		//open up streams and sockets
+		
 		try {
 			socket = new Socket(ip, port);
 		}
@@ -402,7 +524,7 @@ public class Torrent {
 			return;
 		}
 
-		//DISCARD BITFIELD
+		//DISCARD BITFIELD - not needed
 
 		//send interested
 
@@ -426,21 +548,23 @@ public class Torrent {
 		}
 
 
-
+		//check for unchoke from peer
+		
 		if(messages[4] == 1){
 			System.out.println("Unchoke complete");
 			peer.setUnchoked(true);
 		}
 
 		trackerResponse.clear();
-
+		
+		//Send event started to tracker
 		setTrackerURLEventStarted();
 		sendRequestToTracker();
 
 		//printTrackerResponse();
 
 
-
+		//the array we will use for each piece before we verify its hash and then write the data to the file.
 		byte[] pieceArray = new byte[torrentInfo.piece_length];
 		ByteBuffer pieceBuffer = ByteBuffer.wrap(pieceArray);
 		
@@ -451,16 +575,18 @@ public class Torrent {
 		
 		long startTime = System.currentTimeMillis();
 		
+		//this byte array is for each block header, which is 13 bytes.
 		byte[] test = new byte[13];
 		ByteBuffer testBuffer = ByteBuffer.wrap(test);
 		
-
+		//download loop
 		while(left > 0){
 			
+			//clear buffers
 			pieceBuffer.clear();
-			
 			testBuffer.clear();
 
+			//send request for block
 			try {
 				sendRequest();
 			} catch (IOException e) {
@@ -470,7 +596,8 @@ public class Torrent {
 			
 			
 			Thread.sleep(1000);
-
+			
+			//read block header
 			try {
 				if(left >= LENGTH)
 					System.out.println(input.read(test));
@@ -488,7 +615,7 @@ public class Torrent {
 			
 			
 			
-			
+			//read data to piece array
 			try {
 				if(left < LENGTH){
 					 input.read(pieceArray, 0, left);
@@ -506,7 +633,7 @@ public class Torrent {
 			
 			
 		
-			
+			//increment values
 			this.downloaded += LENGTH;
 			this.left -= LENGTH;
 
@@ -514,25 +641,24 @@ public class Torrent {
 			
 			//messagesBuffer.clear();
 
-
+			//clear buffer for header
 			testBuffer.clear();
-
+			
+			//alternate between first and second block.
 			this.firstBlock = !this.firstBlock;
 
 			if(this.firstBlock){
 				
 				//check hash
 				
-				
-				
 				boolean check = (checkArrayEquality(getPieceHash(pieceArray), torrentInfo.piece_hashes[index].array()));
-				//System.out.println(check);
-				//System.out.println("CHECK HASH");
 				
+				//hash is verified. Write to file and increment piece index.
 				if(check){
 					System.out.println("Hash verified");
 					fos.write(pieceArray);
 					index++;
+				//Hash failed. Download this piece again.	
 				}else{
 					System.out.println("Hash failed...reattempting piece download");
 					this.downloaded -= LENGTH*2;
@@ -550,10 +676,12 @@ public class Torrent {
 
 		}
 		
+		//at the end of the download we have the time it took to download.
 		long downloadTime = System.currentTimeMillis() - startTime;
 		
 		System.out.println("Download finished in " + downloadTime + "ms.");
 		
+		//send completed message to tracker
 		setTrackerURLEventCompleted();
 		sendRequestToTracker();
 		
@@ -563,6 +691,12 @@ public class Torrent {
 
 	}
 	
+	/**
+	 * Given a piece array, get its SHA-1 hash to verify it.
+	 * 
+	 * @param data - the raw bytes for the piece
+	 * @return a byte array of size 20 with the SHA-1 hash sequence.
+	 */
 	public byte[] getPieceHash(byte[] data){
 		
 		MessageDigest md = null;
@@ -578,7 +712,15 @@ public class Torrent {
 		
 	}
 
-	//for checking responses from peer
+	/**
+	 * see if two byte arrays are equal.
+	 * 
+	 * used for checking responses from peer
+	 * as well as for verifying a piece's SHA-1 hash.
+	 * @param one - one byte array
+	 * @param two - another byte array
+	 * @return true if the arrays are equal
+	 */
 	public boolean checkArrayEquality(byte[] one, byte[] two){
 		
 		if (one.length != two.length){
@@ -598,7 +740,11 @@ public class Torrent {
 		return true;
 	}
 	
-	//send a handshake to the peer
+	/**
+	 * send a handshake message to the peer
+	 * 
+	 * @throws IOException
+	 */
 	public void sendHandshake() throws IOException{
 		ByteBuffer handshake = ByteBuffer.allocate(68);
 		
@@ -617,6 +763,11 @@ public class Torrent {
 		
 	}
 	
+	/**
+	 * send a have message to the peer
+	 * 
+	 * @throws IOException
+	 */
 	public void sendHave() throws IOException{
 		
 		ByteBuffer buffer = ByteBuffer.allocate(9);
@@ -629,6 +780,11 @@ public class Torrent {
 		
 	}
 	
+	/**
+	 * Send a request message to the peer for a new block.
+	 * 
+	 * @throws IOException
+	 */
 	public void sendRequest() throws IOException{
 		
 		int length; 
@@ -661,19 +817,35 @@ public class Torrent {
 		
 	}
 	
-	//send an unchoke message to the peer
+	/**
+	 * send an unchoke message to the peer.
+	 * The peer will be sending this message to us
+	 * to let us know that we can download from it.
+	 * 
+	 * @throws IOException
+	 */
 	public void sendUnchoke() throws IOException{
 		ByteBuffer buffer = UNCHOKE;
 		sendMessage(buffer);
 	}
 	
-	//send an interested message to the peer
+	/**
+	 * send an interested message to the peer. Lets the peer know that we 
+	 * want to be unchoked.
+	 * 
+	 * @throws IOException
+	 */
 	public void sendInterested() throws IOException{
 		ByteBuffer buffer = INTERESTED;
 		sendMessage(buffer);
 	}
 	
-	//method for sending some message to peer
+	/**
+	 * method for sending some message to peer
+	 * 
+	 * @param message - the bytebuffer that we want to send to the peer
+	 * @throws IOException
+	 */
 	public void sendMessage(ByteBuffer message) throws IOException{
 		if(!socket.isConnected()){
 			System.out.println("socket not connected");
@@ -684,12 +856,19 @@ public class Torrent {
 	}
 	
 	
-	//For debugging. Printing the map that is received from the tracker
+	/**
+	 * For debugging. Printing the map that is received from the tracker
+	 */
 	public void printTrackerResponse(){
 		ToolKit.print(trackerResponse);
 	}
 	
-	//generate a random 20 byte/char ID to be my peer ID
+	/**
+	 * generate a random 20 byte/char ID to be my peer ID. We need this for
+	 * the tracker communication.
+	 * 
+	 * @return a string of 20 random letters
+	 */
 	private String generateRandomID(){
 		
 		StringBuilder sb = new StringBuilder(20);
@@ -711,7 +890,13 @@ public class Torrent {
 	}
 	
 	
-	//Encodes the info_hash value from the torrent info into the hex string that is appended to the URL
+	/**
+	 * Encodes the info_hash value from the torrent info into the hex 
+	 * string that is appended to the URL.
+	 * 
+	 * @param bytes - the info_hash byte array from the torrent info object.
+	 * @return the encoded string that we attach to the tracker URL
+	 */
 	private String encodeInfoHash(byte[] bytes) {
 		StringBuilder sb = new StringBuilder();
 		final Formatter formatter = new Formatter(sb);
